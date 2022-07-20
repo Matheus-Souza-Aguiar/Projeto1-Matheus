fd8dd429a7f112c1ebe086f22fdc3f79c5d9dce9
Leonardo <leonardo.ienke@iliossistemas.com>
2022-07-20 13:24:08 -0300

    Correção do erro de divisão por zero.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from select import select
-from selectors import SelectSelector
 
 cont = 'sim'
 
@@ -20,23 +19,23 @@ def divide(parametro1, parametro2):
 
 
 def subtrai(parametro1, parametro2):
-<<<<<<< HEAD
     return parametro1 - parametro2
 
 
 def chama_operacao(select):
-    select = int(input('1 - somar\n\n2 - multiplicar\n\n3 - dividir\n\n4 - subtrair\nInforme a operação? '))
-    return select
+    return int(input('1 - somar\n\n2 - multiplicar\n\n3 - dividir\n\n4 - subtrair\nInforme a operação? '))
+
 
 SELECAO = {1: soma, 2: multiplica, 3: divide, 4: subtrai}
+
 
 def valor_1():
     return float(input(f'Informe o primeiro valor que você deseja calcular: '))
 
 
+
 def valor_2():
     return float(input(f'Informe o segundo valor que você deseja calcular: '))
-
 
 def valor_3(c):
     return float(c)
@@ -51,14 +50,21 @@ def continua_conta(parametro3):
     a = chama_operacao(select)
     parametro1 = parametro3
     parametro2 = valor_2()
+    if parametro2 == 0:
+        valor0 = input('Não é possivel dividir por 0. Deseja fazer uma nova conta ou informar outro valor? Nova conta(n)/Outro valor(v): ')
+        if valor0 == 'n':
+            centro()
+        elif valor0 == 'v':
+            parametro2 = valor_2()
     parametro3 = devolve_resultado(a, parametro1, parametro2)
     print(parametro3)
 
-    seg = input('Deseja continuar? sim/nao ' )
-    if seg == 'sim':
+    seg = input('Deseja continuar? s/n ')
+    if seg == 's':
         continua_conta(parametro3)
-    elif seg == 'nao':
+    elif seg == 'n':
         centro()
+
 
 def centro():
     while 'sim'.title():
@@ -68,72 +74,24 @@ def centro():
         a = chama_operacao(select)
         parametro1 = valor_1()
         parametro2 = valor_2()
+
+        if parametro2 == 0:
+            valor0 = input('Não é possivel dividir por 0. Deseja fazer uma nova conta ou informar outro valor? Nova conta(n)/Outro valor(v): ')
+            if valor0 == 'n':
+                centro()
+            elif valor0 == 'v':
+                parametro2 = valor_2()
+
         parametro3 = devolve_resultado(a, parametro1, parametro2)
         print(parametro3)
 
-        seg = input('Deseja continuar, fazer nova conta ou finalizar? continuar/nova conta/ finalizar  ')
-        if seg == 'continuar':
+        seg = input('Deseja continuar, fazer nova conta ou finalizar? continuar(c)/nova conta(n)/ finalizar(f):  ')
+        if seg == 'c':
             continua_conta(parametro3)
-        elif seg == 'nova conta':
+        elif seg == 'n':
             centro()
-        elif seg == 'finalizar':
+        elif seg == 'f':
             break
 
 
-=======
-    return parametro1- parametro2
-Selececao = {1: soma, 2: multiplica, 3: divide, 4: subtrai}
-
-def chama_operacao(select):
-    select = int(input('1 - somar\n\n2 - multiplicar\n\n3 - dividir\n\n4 - subtrair\nInforme a operação? '))
-    return select
-
-def valor_1():
-    return float(input(f'Informe o primeiro valor que você deseja calcular: '))
-
-def valor_2():
-    return float(input(f'Informe o segundo valor que você deseja calcular: '))
-
-def valor_3(c):
-    return float(c)
-
-def devolve_resultado(parametro1,parametro2):
-    result = SelectSelector[select](parametro1, parametro2)
-    return result
-
-
-def continua_conta():
-    chama_operacao(select)
-    parametro1 = valor_3(parametro3)
-    parametro2 = valor_2()
-    parametro3 = devolve_resultado
-    return print(devolve_resultado(parametro1, parametro3))
-
-    seg = input('Deseja continuar o calculo? sim/nao:  ')
-    if seg == 'sim':
-        continua_conta()
-    else:
-        return ''
-
-def centro():
-        while 'sim'.title():
-            
-            print('Calculadora 1.0\n')
-            
-            chama_operacao(select)
-            parametro1 = valor_1()
-            parametro2 = valor_2()
-            parametro3 = devolve_resultado(parametro1,parametro2)
-            print(devolve_resultado(parametro1,parametro2))
-
-
-            seg = input('Deseja continaur, fazer nova conta ou finalizar? continaur/nova conta/finalizar  ')
-            if seg == 'continaur':
-                print(continua_conta())
-            elif seg == 'nova conta':
-                centro()
-            elif seg == 'finalizar':
-                  break
-                            
->>>>>>> 8e5a92bc1fb5ff4589ed71d6740e0ff400d21740
 centro()
