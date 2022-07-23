e2fa71aa55d96f377181a83997ca624bd4ddbc46
Matheus Souza Aguiar <matheus.aguiar0511@gmail.com>
2022-07-23 01:03:06 -0300

    Repetição do erro de divisão por zero. alteração no nome das variáveis.

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from select import select
 
 
-def soma(paramtro1, parametro2):
-    return paramtro1 + parametro2
+def soma(primeiro_parametro, segundo_parametro):
+    return primeiro_parametro + segundo_parametro
 
 
-def multiplica(parametro1, parametro2):
-    return parametro1 * parametro2
+def multiplica(primeiro_parametro, segundo_parametro):
+    return primeiro_parametro * segundo_parametro
 
 
-def divide(parametro1, parametro2):
-    try:
-        return parametro1 / parametro2
-    except ZeroDivisionError:
-        return 'Não é possivel dividir por zero'
+def divide(primeiro_parametro, segundo_parametro):
+       try:
+           if segundo_parametro == 0.0:
+               segundo_parametro = repeticao_de_zero()
+           return primeiro_parametro / segundo_parametro
+       except ZeroDivisionError:
+           'Não é possivel dividir por 0'
+
+
+def repeticao_de_zero():
+    valor_de_zero = input('Não é possivel dividir por 0.\nDeseja informar outro valor? sim(s)/não(n): ')
+    if valor_de_zero == 's':
+        novo_valor_do_segundo_parametro = segundo_valor_do_parametro()
+        if novo_valor_do_segundo_parametro == 0.0:
+             return repeticao_de_zero()
+        return novo_valor_do_segundo_parametro
+    elif valor_de_zero == 'n':
+        centro()
 
 
 def subtrai(parametro1, parametro2):
@@ -27,37 +40,31 @@ def chama_operacao(select):
 SELECAO = {1: soma, 2: multiplica, 3: divide, 4: subtrai}
 
 
-def valor_1():
+def primeiro_valor_do_parametro():
     return float(input(f'Informe o primeiro valor que você deseja calcular: '))
 
 
-def valor_2():
+def segundo_valor_do_parametro():
     return float(input(f'Informe o segundo valor que você deseja calcular: '))
 
 
-def devolve_resultado(operador, parametro1, parametro2):
-    result = SELECAO[operador](parametro1, parametro2)
+def devolve_resultado(operador, primeiro_parametro, segundo_parametro):
+    result = SELECAO[operador](primeiro_parametro, segundo_parametro)
     return result
 
 
-def continua_conta(parametro3):
+def continua_conta(terceiro_parametro):
     operador = chama_operacao(select)
-    parametro1 = parametro3
-    parametro2 = valor_2()
-    if parametro2 == 0:
-        valor0 = input('Não é possivel dividir por 0. Deseja fazer uma nova conta ou informar outro valor? Nova conta(n)/Outro valor(v): ')
-        if valor0 == 'n':
-            centro()
-        elif valor0 == 'v':
-            parametro2 = valor_2()
-    parametro3 = devolve_resultado(operador, parametro1, parametro2)
-    print(parametro3)
+    primeiro_parametro = terceiro_parametro
+    segundo_parametro = segundo_valor_do_parametro()
+    terceiro_parametro = devolve_resultado(operador, primeiro_parametro, segundo_parametro)
+    print(terceiro_parametro)
 
     seg = input('Deseja continuar? s/n ')
     if seg == 's':
-        continua_conta(parametro3)
+        continua_conta(terceiro_parametro)
     elif seg == 'n':
-          centro()
+        centro()
 
 
 def centro():
@@ -66,22 +73,14 @@ def centro():
         print('Calculadora 1.0\n')
 
         operador = chama_operacao(select)
-        parametro1 = valor_1()
-        parametro2 = valor_2()
-
-        if parametro2 == 0:
-            valor0 = input('Não é possivel dividir por 0. Deseja fazer uma nova conta ou informar outro valor? Nova conta(n)/Outro valor(v): ')
-            if valor0 == 'n':
-                centro()
-            elif valor0 == 'v':
-                parametro2 = valor_2()
-
-        parametro3 = devolve_resultado(operador, parametro1, parametro2)
-        print(parametro3)
+        primeiro_parametro = primeiro_valor_do_parametro()
+        segundo_parametro = segundo_valor_do_parametro()
+        terceiro_parametro = devolve_resultado(operador, primeiro_parametro, segundo_parametro)
+        print(terceiro_parametro)
 
         seg = input('Deseja continuar, fazer nova conta ou finalizar? continuar(c)/nova conta(n)/ finalizar(f):  ')
         if seg == 'c':
-            continua_conta(parametro3)
+            continua_conta(terceiro_parametro)
         elif seg == 'n':
             centro()
         elif seg == 'f':
